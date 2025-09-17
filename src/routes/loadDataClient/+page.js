ebcd45151aee13eb0729414637513fe7735a861f
Max Frischknecht <info@maxfrischknecht.ch>
2025-09-17 11:56:57 +0200

    edd public prefix

--- src/routes/loadDataClient/+page.js
+++ src/routes/loadDataClient/+page.js
@@ -1,6 +1,7 @@
 // This file can be executed in the browser, use only public keys!
 // They can be imported from an .env file in the project root like so: 
-// import { API_KEY } from '$env/static/public';
+// must be prefixed with PUBLIC_
+// import { PUBLIC_API_KEY } from '$env/static/public';
 
 export async function load({fetch}) {
     // Load a static file inside 'static/data/' in the client (browser) 
